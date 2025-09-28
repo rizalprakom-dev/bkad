dedfc0c7fa7c39a8520cf1645df131ac6ae585cc
rizalprakom-dev <rizal.prakom@gmail.com>
2025-09-29 05:31:22 +0700

    Create migration for menus table

---
+++ app/Database/Migrations/CreateMenusTable.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateMenusTable extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id'        => ['type' => 'INT', 'auto_increment' => true],
+            'title'     => ['type' => 'VARCHAR', 'constraint' => 100],
+            'url'       => ['type' => 'VARCHAR', 'constraint' => 255],
+            'parent_id' => ['type' => 'INT', 'null' => true],
+            'order'     => ['type' => 'INT', 'default' => 0],
+            'is_active' => ['type' => 'TINYINT', 'constraint' => 1, 'default' => 1],
+        ]);
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('menus');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('menus');
+    }
+}
